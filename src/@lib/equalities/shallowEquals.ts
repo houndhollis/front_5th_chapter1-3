3947cfdad1fac9f2059d5f7a6be7484c12a749be
houndhollis <houndhollis9797@gmail.com>
2025-04-06 02:11:18 +0900

    feat: 얕은 비교 추가

--- src/@lib/equalities/shallowEquals.ts
+++ src/@lib/equalities/shallowEquals.ts
@@ -1,3 +1,26 @@
+const isObjet = (object: unknown): object is Record<string, unknown> => {
+  return typeof object === "object" && object !== null;
+};
+
 export function shallowEquals<T>(objA: T, objB: T): boolean {
-  return objA === objB;
+  if (objA === objB) {
+    return true;
+  }
+
+  if (Array.isArray(objA) && Array.isArray(objB)) {
+    if (objA.length !== objB.length) {
+      return false;
+    }
+
+    return objA.every((val, i) => val === objB[i]);
+  }
+
+  if (isObjet(objA) && isObjet(objB)) {
+    if (Object.keys(objA).length !== Object.keys(objB).length) {
+      return false;
+    }
+    return Object.keys(objA).every((key) => objA[key] === objB[key]);
+  }
+
+  return false;
 }
