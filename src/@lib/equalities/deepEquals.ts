8ca0ffb138e32867fa02f3e55d243b25849db2b5
houndhollis <houndhollis9797@gmail.com>
2025-04-06 23:51:50 +0900

    feat: deepEquasl 추가

--- src/@lib/equalities/deepEquals.ts
+++ src/@lib/equalities/deepEquals.ts
@@ -1,3 +1,48 @@
+const isObject = (object: unknown): object is Record<string, unknown> => {
+  return typeof object === "object" && object !== null;
+};
+
 export function deepEquals<T>(objA: T, objB: T): boolean {
-  return objA === objB;
+  if (objA === objB) {
+    return true;
+  }
+
+  if (Array.isArray(objA) && Array.isArray(objB)) {
+    if (objA.length !== objB.length) {
+      return false;
+    }
+
+    const flatA = objA.flat(Infinity);
+    const flatB = objB.flat(Infinity);
+
+    return flatA.every((val, i) => val === flatB[i]);
+  }
+
+  if (isObject(objA) && isObject(objB)) {
+    if (Object.keys(objA).length !== Object.keys(objB).length) {
+      return false;
+    }
+
+    const sortObjA = Object.keys(objA)
+      .sort()
+      .reduce(
+        (obj, key) => {
+          obj[key] = objA[key];
+          return obj;
+        },
+        {} as Record<string, unknown>,
+      );
+    const sortObjB = Object.keys(objB)
+      .sort()
+      .reduce(
+        (obj, key) => {
+          obj[key] = objB[key];
+          return obj;
+        },
+        {} as Record<string, unknown>,
+      );
+    return JSON.stringify(sortObjA) === JSON.stringify(sortObjB);
+  }
+
+  return false;
 }
